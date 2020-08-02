570896cb15302fc2185d310933a3851e7b0e3787
Simon Geier <super.zockerhd@web.de>
2020-08-02 18:52:16 +0200

    Trying out std::initializer_list

--- TestProject/src/main.cpp
+++ TestProject/src/main.cpp
@@ -1,44 +1,76 @@
 #include "pch.h"
 
 
-//long multithreadedSum = 0;
-std::atomic<long> multithreadedSum(0);
-
-void SumNumbers(const std::vector<int>& toBeSummed, int idxStart, int idxEnd)
+namespace sstd
 {
-	for (int i = idxStart; i <= idxEnd; ++i)
+	template<typename T, size_t S>
+	class Array
 	{
-		multithreadedSum += toBeSummed[i];
-	}
+	public:
+		Array() : nextIdx(0) {}
+		Array(std::initializer_list<T> initList)
+			: nextIdx(0)
+		{
+			for (auto e : initList)
+			{
+				this->Add(e);
+			}
+		}
+
+		void Add(const T& elem)
+		{
+			m_Arr[nextIdx] = elem;
+			++nextIdx;
+		}
+
+		constexpr size_t Size() const { return S; }
+
+		T& operator[](const size_t index) { return m_Arr[index]; }
+		const T& operator[](const size_t index) const { return m_Arr[index]; }
+
+		const Array operator=(const Array& other)
+		{
+			for (unsigned int i = 0; i < S; ++i)
+			{
+				other[i] = m_Arr[i];
+			}
+		}
+
+		T* Data() { return m_Arr; }
+		const T* Data() const { return m_Arr; }
+
+		//unsigned int begin()
+		//{
+		//	return 0;
+		//}
+		//unsigned int end()
+		//{
+		//	return this->Size();
+		//}
+
+	private:
+		T m_Arr[S];
+		int nextIdx;
+	};
 }
 
 
 int main()
 {
-	std::vector<int> toBeSummed;
-	for (int i = 0; i < 30000000; ++i)
+	//std::vector<int> vec{1, 4, 6, 2, 7, 3, 8, 3, 65, 23, 4};
+
+	//for (int a : vec)
+	//{
+	//	std::cout << a << '\n';
+	//}
+	
+	sstd::Array<int, 10> arr{ 1, 4, 2, 8, 3, 5, 5, 8, 2, 4 };
+
+	for (unsigned int i = 0; i < arr.Size(); ++i)
 	{
-		toBeSummed.emplace_back(std::rand());
+		std::cout << arr[i] << '\n';
 	}
 
-	long sum = 0;
-	for (const int num : toBeSummed)
-	{
-		sum += num;
-	}
-	printf("The sum without multithreading is %d\n", sum);
-
-	//-----------------MULTITHREADING-----------------
-
-	std::thread t1(SumNumbers, toBeSummed, 0, 9999999);
-	std::thread t2(SumNumbers, toBeSummed, 10000000, 19999999);
-	std::thread t3(SumNumbers, toBeSummed, 20000000, 29999999);
-
-	t1.join();
-	t2.join();
-	t3.join();
-
-	printf("The sum with multithreading is    %d\n", multithreadedSum.load());
 }
 
 
