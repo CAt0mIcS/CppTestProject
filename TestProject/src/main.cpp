9a26fe2a0ea895b2a4cf4d7f849fae4b7eb72520
Simon <simon_geier@web.de>
2020-08-19 21:35:06 +0200

    Used SetFileTime to sort pictures by date

--- TestProject/src/main.cpp
+++ TestProject/src/main.cpp
@@ -1,35 +1,152 @@
 #include "pch.h"
 
 
-class Entity
-{
-public:
-	int x, y, z;
-	std::string str;
+//struct DateTime
+//{
+//	int year;
+//	int month;
+//	int dayOfWeek;
+//	int day;
+//	int hour;
+//	int minute;
+//	int second;
+//	int milisecond;
+//
+//	friend std::ostream& operator<<(std::ostream& os, const DateTime& dt);
+//};
 
-	Entity(int x, int y, int z, const std::string& str)
-		:x(x), y(y), z(z), str(str) {}
+//DateTime g_Dt;
+
+struct SystemTimes
+{
+	SYSTEMTIME creationTime, lastAccessTime, lastWriteTime;
 };
 
-
-template<typename T>
-void AreEqual(T* obj1, T* obj2, const char* message)
+SystemTimes GetWindowsCreationDate(const std::string& path)
 {
-	char* pObj1 = (char*)obj1;
-	char* pObj2 = (char*)obj2;
 
-	for (int i = 0; i < sizeof(T); ++i)
+	HANDLE hFile = CreateFile(path.c_str(), GENERIC_READ, 0, 0, OPEN_EXISTING, 0, 0);
+
+	if (!hFile || hFile == INVALID_HANDLE_VALUE)
+		return { 0 };
+
+	FILETIME creationDate;
+	FILETIME lastAccessDate;
+	FILETIME lastWriteDate;
+
+	BOOL success = GetFileTime(hFile, &creationDate, &lastAccessDate, &lastWriteDate);
+
+	if (!success)
+		return { 0 };
+
+	SYSTEMTIME creationTime;
+	SYSTEMTIME lastAccessTime;
+	SYSTEMTIME lastWriteTime;
+	FileTimeToSystemTime(&creationDate, &creationTime);
+	FileTimeToSystemTime(&creationDate, &lastAccessTime);
+	FileTimeToSystemTime(&creationDate, &lastWriteTime);
+
+	CloseHandle(hFile);
+	return { creationTime, lastAccessTime, lastWriteTime };
+}
+
+BOOL SetWindowsCreationDate(const SystemTimes& dt, const std::string& path)
+{
+	HANDLE hFile = CreateFile(path.c_str(), GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0);
+
+	if (!hFile || hFile == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	FILETIME cDate;
+	FILETIME lADate;
+	FILETIME lWDate;
+	SystemTimeToFileTime(&dt.creationTime, &cDate);
+	SystemTimeToFileTime(&dt.lastAccessTime, &lADate);
+	SystemTimeToFileTime(&dt.lastWriteTime, &lWDate);
+
+	BOOL success = SetFileTime(hFile, &cDate, &lADate , &lWDate);
+	if (!success)
 	{
-		if ((int)pObj1[i] != (int)pObj2[i])
-			__debugbreak();
+		CHAR buffer[256];
+		std::cout << FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, 0, GetLastError(), 0, buffer, sizeof(buffer), 0) << '\n';
 	}
+
+	CloseHandle(hFile);
+	return success;
+}
+
+inline bool FileExists(const std::string& name) {
+	struct stat buffer;
+	return (stat(name.c_str(), &buffer) == 0);
+}
+
+std::vector<std::string> GetBaseFileDirs()
+{
+	std::vector<std::string> dirs{};
+	dirs.reserve(100);
+	std::ifstream reader("D:\\dev\\Python\\Projects\\SortPicturesByDate\\output.txt");
+
+	std::string line;
+	while (std::getline(reader, line))
+	{
+		dirs.emplace_back(std::move(line));
+	}
+
+	return dirs;
+}
+
+std::string GetFileName(const std::string& path)
+{
+	std::string filename = path;
+	unsigned short lastId = path.find_last_of("\\");
+
+	filename.replace(0, lastId + 1, "");
+
+	return filename;
+}
+
+bool CopyFiles(unsigned int minYear, unsigned int maxYear, const SystemTimes& dates, const std::string& path)
+{
+	if (dates.creationTime.wYear > maxYear || dates.creationTime.wYear < minYear)
+		return false;
+
+	const char* months[] = { "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" };
+	std::stringstream ss;
+
+	std::stringstream ss2;
+	ss2 << "F:\\Test\\" << dates.creationTime.wYear << "\\" << months[dates.creationTime.wMonth - 1];
+
+	ss << "copy " << "\"" << path << "\" " << ss2.str();
+	bool success = !(bool)system(ss.str().c_str());
+
+	std::string filename = GetFileName(path);
+
+	if(success)
+		std::cout << SetWindowsCreationDate(dates, ss2.str() + "\\" + filename) << '\n';
+
+	return success;
+}
+
+void MoveTemp()
+{
+	int runs = 0;
+	for (auto& dir : GetBaseFileDirs())
+	{
+		SystemTimes dates = GetWindowsCreationDate(dir);
+
+		//TODO: Copy files to ordered directory using the creationDate
+		if (!CopyFiles(2016, 2020, dates, dir))
+		{
+			std::cout << "Failed to copy file " << dir << '\n';
+		}
+		++runs;
+	}
+	std::cout << "Program ran through " << runs << " file(s)\n";
+	//Ran 521 times
 }
 
 
 int main()
 {
-	Entity e (1, 2, 3, "H");
-	Entity e2(1, 2, 3, "H");
-
-	AreEqual(&e, &e2, "Messaage);");
+	MoveTemp();
 }
