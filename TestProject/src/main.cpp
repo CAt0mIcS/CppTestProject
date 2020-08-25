110eabfe7ebfb280fd1e1d7f796e1241156f9bcb
Simon Geier <super.zockerhd@web.de>
2020-08-25 21:25:27 +0200

    Played around with structured bindings for custom classes

--- TestProject/src/main.cpp
+++ TestProject/src/main.cpp
@@ -1,18 +1,54 @@
 #include "pch.h"
 
-typedef struct
+
+class UserEntry
 {
-	unsigned int t : 1;
-} st_t;
+public:
+	UserEntry(const std::string& name, unsigned int age) : m_Name(name), m_Age(age), m_CacheEntry(0) {}
+
+	void Load() {}
+
+	const std::string& GetName() const { return m_Name; }
+	unsigned int GetAge() const { return m_Age; }
+
+private:
+	std::string m_Name;
+	unsigned int m_Age;
+	size_t m_CacheEntry;
+};
+
+
+
+namespace std 
+{
+	template <> struct tuple_size<UserEntry> : integral_constant<size_t, 2> { };
+	template <> struct tuple_element<0, UserEntry> { using type = std::string; };
+	template <> struct tuple_element<1, UserEntry> { using type = unsigned; };
+}
+
+//Couldn't get this to work...
+//template<>
+//const std::string& get<1>(const UserEntry& u) { return u.GetName(); }
+//
+//template<>
+//unsigned get<0>(const UserEntry& u) { return u.GetAge(); }
+
+//This however works...
+template<size_t I>
+auto get(const UserEntry& u)
+{
+	if constexpr (I == 0) return u.GetName();
+	else if constexpr (I == 1) return u.GetAge();
+}
+
 
 
 int main()
 {
-	st_t tStruct{};
-	tStruct.t = 5;
-	//tStruct.v = 2;
-	std::cout << "Size: " << sizeof(tStruct) << '\n';
-	std::cout << "t: " << tStruct.t << '\n';
-	//std::cout << "v: " << tStruct.v << '\n';
+	UserEntry e("Simon", 15);
+
+	auto [name, age] = e;
+
+	std::cout << "Name: " << name << "\nAge: " << age << '\n';
 
 }
