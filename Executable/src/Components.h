916ec69573e1ea58b3839e9d569daa4b10129bc7
Simon <simon_geier@web.de>
2020-12-16 19:37:12 +0100

    Initial commit

---
+++ Executable/src/Components.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+
+struct TagComponent
+{
+	std::string tag;
+};
+
+struct TransformComponent
+{
+
+};
+
+struct SpriteRenderComponent
+{
+
+};
+
+struct RenderComponent
+{
+
+};
+
+struct NativeScriptingComponent
+{
+
+};
