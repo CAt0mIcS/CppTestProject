55fc3397133eb9f925df3d18c4b8692f0e77ce82
Simon <simon_geier@web.de>
2020-12-22 21:05:30 +0100

    Reverted test

--- EntityComponentSystem/src/ECS/View.h
+++ EntityComponentSystem/src/ECS/View.h
@@ -3,21 +3,6 @@
 #include "Storage.h"
 #include <tuple>
 
-template<bool Test, typename T1, typename T2>
-struct conditional
-{
-	using type = T1;
-};
-
-
-template<typename T1, typename T2>
-struct conditional<false, T1, T2>
-{
-	using type = T2;
-};
-
-template<bool Test, typename T1, typename T2>
-using conditional_t = typename conditional<Test, T1, T2>::type;
 
 namespace ECS
 {
@@ -27,7 +12,7 @@ namespace ECS
 	private:
 		// If the Component (Comp) is const then PoolType will be "const Storage<std::remove_const_t<Comp>>" else just "Storage<Comp>"
 		template<typename Comp>
-		using PoolType = conditional_t<std::is_const_v<Comp>, const Storage<std::remove_const_t<Comp>>, Storage<Comp>>;
+		using PoolType = std::conditional_t<std::is_const_v<Comp>, const Storage<std::remove_const_t<Comp>>, Storage<Comp>>;
 
 	public:
 		View(Storage<Component>&... component)
