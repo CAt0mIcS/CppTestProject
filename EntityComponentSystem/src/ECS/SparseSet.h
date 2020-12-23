c172fdcb88ffe65d7cd9c06e44b82a775c272f3d
Simon <simon_geier@web.de>
2020-12-23 15:24:49 +0100

    Almost working iterators

--- EntityComponentSystem/src/ECS/SparseSet.h
+++ EntityComponentSystem/src/ECS/SparseSet.h
@@ -10,6 +10,105 @@ namespace ECS
 	struct SparseSet
 	{
 	public:
+		class Iterator
+		{
+		public:
+			Iterator(const std::vector<Entity>& packed, uint32_t index)
+				: m_Packed(packed), m_Idx(index) {}
+
+			Iterator& operator++()
+			{
+				return --m_Idx, *this;
+			}
+
+			Iterator& operator--()
+			{
+				return ++m_Idx, *this;
+			}
+
+			Iterator& operator--(int)
+			{
+				Iterator orig = *this;
+				return operator--(), orig;
+			}
+
+			Iterator& operator+=(uint32_t value)
+			{
+				m_Idx -= value;
+				return *this;
+			}
+
+			Iterator& operator-=(uint32_t value)
+			{
+				m_Idx += value;
+				return *this;
+			}
+
+			Iterator operator+(uint32_t value) const
+			{
+				Iterator copy = *this;
+				return (copy += value);
+			}
+
+			Iterator operator-(uint32_t value) const
+			{
+				Iterator copy = *this;
+				return (copy -= value);
+			}
+
+			const Entity& operator[](size_t idx) const
+			{
+				const size_t pos = m_Idx - idx - 1;
+				return m_Packed[pos];
+			}
+
+			bool operator==(const Iterator& other) const
+			{
+				return other.m_Idx == m_Idx;
+			}
+
+			bool operator!=(const Iterator& other) const
+			{
+				return !(*this == other);
+			}
+
+			bool operator<(const Iterator& other) const
+			{
+				return m_Idx > other.m_Idx;
+			}
+
+			bool operator>(const Iterator& other) const
+			{
+				return m_Idx < other.m_Idx;
+			}
+
+			bool operator<=(const Iterator& other) const
+			{
+				return !(*this > other);
+			}
+
+			bool operator>=(const Iterator& other) const
+			{
+				return !(*this < other);
+			}
+
+			const Entity* operator->() const
+			{
+				const size_t pos = m_Idx - 1;
+				return &m_Packed[pos];
+			}
+
+			const Entity& operator*() const
+			{
+				return *operator->();
+			}
+
+		private:
+			const std::vector<Entity>& m_Packed;
+			size_t m_Idx;
+		};
+
+	public:
 		Entity Search(Entity entity)
 		{
 			if (m_Sparse[entity] < n && m_Packed[m_Sparse[entity]] == entity)
@@ -35,9 +134,24 @@ namespace ECS
 			--n;
 		}
 
-		uint32_t Index(Entity entity) const
+		size_t Index(Entity entity) const
 		{
 			return m_Sparse[entity];
+		}
+
+		size_t Size() const
+		{
+			return m_Packed.size();
+		}
+
+		Iterator begin() const
+		{
+			return Iterator{ m_Packed, m_Packed.size() };
+		}
+
+		Iterator end() const
+		{
+			return Iterator{ m_Packed, {} };
 		}
 
 	private:
--- EntityComponentSystem/src/ECS/View.h
+++ EntityComponentSystem/src/ECS/View.h
@@ -15,10 +15,82 @@ namespace ECS
 		using PoolType = std::conditional_t<std::is_const_v<Comp>, const Storage<std::remove_const_t<Comp>>, Storage<Comp>>;
 
 	public:
+		class Iterator
+		{
+		public:
+			Iterator(const SparseSet& candidate, SparseSet::Iterator curr)
+				: m_View{ candidate }, m_It{ curr } {}
+
+			Iterator operator++()
+			{
+				while (++m_It != m_View.end());
+				return *this;
+			}
+
+			Iterator operator--()
+			{
+				while (--m_It != m_View.end());
+				return *this;
+			}
+
+			Iterator operator--(int)
+			{
+				Iterator original = *this;
+				return operator--(), original;
+			}
+
+			bool operator==(const Iterator& other) const
+			{
+				return other.m_It == m_It;
+			}
+
+			bool operator!=(const Iterator& other) const
+			{
+				return !(*this == other);
+			}
+
+			const Entity* operator->() const
+			{
+				return m_It.operator->();
+			}
+
+			const Entity& operator*() const
+			{
+				return *operator->();
+			}
+
+		private:
+			const SparseSet& m_View;
+			SparseSet::Iterator m_It;
+		};
+
+	public:
 		View(Storage<Component>&... component)
 			: m_Components{ &component... }
 		{
 
+		}
+
+		Iterator begin() const
+		{
+			const SparseSet& view = Candidate();
+			return Iterator{ view, view.begin() };
+		}
+
+		Iterator end() const
+		{
+			const SparseSet& view = Candidate();
+			return Iterator{ view, view.end() };
+		}
+
+	private:
+		const SparseSet& Candidate() const
+		{
+			return *std::min({ static_cast<const SparseSet*>(std::get<PoolType<Component>*>(m_Components))... }, [](const SparseSet* lhs, const SparseSet* rhs)
+				{
+					return lhs->Size() < rhs->Size();
+				}
+			);
 		}
 
 	private:
