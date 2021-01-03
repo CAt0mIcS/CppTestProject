9be999e5c3128edf52386bc478158f66664ba9db
Simon <simon_geier@web.de>
2021-01-03 17:46:18 +0100

    Reverted project

--- Executable/src/main.cpp
+++ Executable/src/main.cpp
@@ -1,24 +1,7 @@
 #include "pch.h"
-#include "Components.h"
-
-#include <ECS\EntityComponentSystem.h>
 
 
 int main()
 {
-	ECS::Registry registry;
-	registry.Emplace<TagComponent>();
-	registry.Emplace<TransformComponent>();
-	registry.Emplace<SpriteRenderComponent>();
-	registry.Emplace<RenderComponent>();
-	registry.Emplace<NativeScriptingComponent>();
-
-	if (registry.Has<TagComponent>())
-	{
-		std::cout << "Component found\n";
-	}
-	else
-	{
-		std::cout << "Component not found\n";
-	}
+	
 }
