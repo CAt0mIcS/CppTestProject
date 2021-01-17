690769f31a541a5db9a280a7bc64f92015f8ae5f
Simon <simon_geier@web.de>
2021-01-17 18:32:21 +0100

    Reset project for experimentation

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,120 +1,79 @@
-#include "VertexBufferLayout.h"
-#include "VertexBuffer.h"
-#include "Texture.h"
-
-#include "tests/Test.h"
-#include "tests/TestClearColor.h"
-#include "tests/TestTexture2D.h"
-
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
-#include "Timer.h"
-
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-   
+
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+	//window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
+	//Activates VSync
+	glfwSwapInterval(1);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
-    //window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	if (GLEW_OK != glewInit())
+		return -1;
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	glViewport(0, 0, 960, 540);
 
-    //Activates VSync
-    glfwSwapInterval(1);
+	float positions[6] =
+	{
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
 
-    if (GLEW_OK != glewInit()) {
-        return -1;
+	uint32_t indices[] =
+	{
+		0, 1, 2
+	};
 
-    }
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    std::cout << glGetString(GL_VERSION) << '\n';
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-    {
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), indices, GL_STATIC_DRAW);
 
-        //For texture rendering
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.1f, 0.12f, 0.15f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        Renderer renderer;
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
-        //IMGUI
-        ImGui::CreateContext();
-        ImGui_ImplGlfwGL3_Init(window, true);
-        ImGui::StyleColorsDark();
+		glfwSwapBuffers(window);
 
-
-        test::Test* currentTest = nullptr;
-        test::TestMenu* testMenu = new test::TestMenu(currentTest);
-        currentTest = testMenu;
-
-        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
-        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
-
-        while (!glfwWindowShouldClose(window))
-        {
-
-            renderer.SetClearColor();
-            renderer.Clear();
-
-            ImGui_ImplGlfwGL3_NewFrame();
-
-            if (currentTest)
-            {
-                currentTest->OnUpdate(0.0f);
-                currentTest->OnRender();
-                ImGui::Begin("Test");
-
-                if (currentTest != testMenu && ImGui::Button("<-"))
-                {
-                    delete currentTest;
-                    currentTest = testMenu;
-                }
-                currentTest->OnImGuiRender();
-
-                ImGui::End();
-            }
-
-            ImGui::Render();
-            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-            glfwSwapBuffers(window);
-
-            glfwPollEvents();
-        }
-
-        delete currentTest;
-        if(currentTest != testMenu)
-            delete testMenu;
-    }
-
-    ImGui_ImplGlfwGL3_Shutdown();
-    ImGui::DestroyContext();
+		glfwPollEvents();
+	}
 
 
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
