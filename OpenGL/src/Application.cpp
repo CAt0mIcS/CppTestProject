54dd91aaa9440ead90c24de3ced734c121e3af13
Simon <simon_geier@web.de>
2021-01-23 22:16:04 +0100

    VShader uniform

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,35 +10,38 @@
 #include <iostream>
 #include <random>
 
+#include <DirectXMath.h>
+
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 450 core\n"
-"layout (location = 0) in vec3 aPos;\n"
+const char* vertexShaderSource =
+"#version 450\n"
+
+"out gl_PerVertex\n"
+"{\n"
+"	vec4 gl_Position;\n"
+"};\n"
+
+"layout(binding = 0, std140) uniform type_Projection\n"
+"{\n"
+"	mat4 modelViewProj;\n"
+"} Projection;\n"
+
+"layout(location = 0) in vec3 in_var_Position;\n"
+
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+"	gl_Position = Projection.modelViewProj * vec4(in_var_Position, 1.0);\n"
+"}\n";
 const char* fragmentShaderSource = "#version 450 core\n"
-
-"uniform type_TriangleColor\n"
-"{\n"
-"	vec3 color;\n"
-"} TriangleColor;\n"
-
-//"struct TriangleColor\n"
-//"{\n"
-//"	vec3 color;\n"
-//"};\n"
-//"uniform TriangleColor TCol;\n"
-//"\n"
 
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(TriangleColor.color, 1.0);\n"
+"   FragColor = vec4(1.0, 1.0f, 1.0, 1.0);\n"
 "}\n\0";
 
 
@@ -146,37 +149,19 @@ int main()
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_UNIFORM_BUFFER, buffer);
 
-	srand((uint32_t)time(0));
-	std::mt19937 mtEngine;
-	std::uniform_real_distribution<float> dist(0.0f, 1.0f);
-
-	float color[] = { 1.0f, dist(mtEngine), dist(mtEngine) };
-	float increments[] = { 0.05f, 0.05f, 0.05f };
-
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		if (color[0] > 1.0f || color[0] < 0.0f)
-			increments[0] = -increments[0];
-
-		if (color[1] > 1.0f || color[1] < 0.0f)
-			increments[1] = -increments[1];
-
-		if (color[2] > 1.0f || color[2] < 0.0f)
-			increments[2] = -increments[2];
-
-		color[0] += increments[0];
-		color[1] += increments[1];
-		color[2] += increments[2];
-
 		glUseProgram(shaderProgram);
 
-		int blockIndex = glGetUniformBlockIndex(shaderProgram, "type_TriangleColor");
+		DirectX::XMMATRIX mat = DirectX::XMMatrixTranslation(0.0f, 1.0f, 0.0f);
+
+		int blockIndex = glGetUniformBlockIndex(shaderProgram, "type_Projection");
 		glUniformBlockBinding(shaderProgram, blockIndex, 1);
 		glBindBufferBase(GL_UNIFORM_BUFFER, 1, buffer);
-		glBufferData(GL_UNIFORM_BUFFER, sizeof(color), color, GL_DYNAMIC_DRAW);
+		glBufferData(GL_UNIFORM_BUFFER, sizeof(mat), &mat, GL_DYNAMIC_DRAW);
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
