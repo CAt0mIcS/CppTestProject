a254642b8fe376e0fb61f89410d2dbe8d42e4094
Simon <simon_geier@web.de>
2021-02-27 22:39:08 +0100

    Reset project

---
+++ Sources/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <type_traits>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <vector>
+#include <array>
