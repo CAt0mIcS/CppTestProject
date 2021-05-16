8d475199a3640148d48d646470801af995d0fbcc
CAt0mIcS <simon_geier@web.de>
2021-05-16 09:43:59 +0200

    Mono working on windows

---
+++ Sources/main.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+
+
+#include <mono/jit/jit.h>
+
+#include <mono/metadata/object.h>
+#include <mono/metadata/environment.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/mono-config.h>
+
+
+void RunStaticMethod(MonoDomain* domain, MonoImage* image, const char* methodDesc)
+{
+	MonoMethodDesc* TypeMethodDesc;
+	TypeMethodDesc = mono_method_desc_new(methodDesc, NULL);
+	if (!TypeMethodDesc)
+		std::cout << "mono_method_desc_new failed" << std::endl;
+
+	// Search the method in the image
+	MonoMethod* method;
+	method = mono_method_desc_search_in_image(TypeMethodDesc, image);
+	if (!method)
+		std::cout << "mono_method_desc_search_in_image failed" << std::endl;
+
+	// run the method
+	std::cout << "Running the static method: " << methodDesc << std::endl;
+	mono_runtime_invoke(method, nullptr, nullptr, nullptr);
+}
+
+
+int main()
+{
+	std::string command = "\"C:/Program Files/Mono/bin/mcs\" Sources/Test.cs -target:library";
+
+	// Compile the script
+	system(command.c_str());
+
+	mono_set_dirs("C:\\Program Files\\Mono\\lib", "C:\\Program Files\\Mono\\etc");
+
+	/*
+	 * Load the default Mono configuration file, this is needed
+	 * if you are planning on using the dllmaps defined on the
+	 * system configuration
+	 */
+	mono_config_parse(NULL);
+
+	MonoDomain* domain = mono_jit_init("Sources/Test.dll");
+
+	MonoAssembly* assembly;
+
+	assembly = mono_domain_assembly_open(domain, "Sources/Test.dll");
+	if (!assembly)
+		exit(2);
+
+	MonoImage* image;
+	image = mono_assembly_get_image(assembly);
+	if (!image)
+		std::cout << "mono_assembly_get_image failed" << std::endl;
+
+	RunStaticMethod(domain, image, "Dog::Type()");
+
+	mono_jit_cleanup(domain);
+}
---
+++ Sources/pch.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <type_traits>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <assert.h>
+#include <optional>
+#include <vector>
+#include <array>
+
+template<typename T>
+using Scope = std::unique_ptr<T>;
+
+template<typename T>
+using Ref = std::shared_ptr<T>;
+
+template<typename T, typename... Args>
+Scope<T> MakeScope(Args&&... args)
+{
+	return std::make_unique<T>(std::forward<Args>(args)...);
+}
+
+template<typename T, typename... Args>
+Ref<T> MakeRef(Args&&... args)
+{
+	return std::make_shared<T>(std::forward<Args>(args)...);
+}
