557ce4f2ab0273c26532d118ac15ea1b24cd8129
CAt0mIcS <simon_geier@web.de>
2021-05-28 14:08:47 +0200

    Message as first parameter

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -17,16 +17,17 @@ public:
 	template<typename... Ts>
 	struct Trace
 	{
-		Trace(Ts&&... ts, std::source_location loc = std::source_location::current())
+		Trace(std::string_view msg, Ts&&... ts, std::source_location loc = std::source_location::current())
 		{
 			std::cout << loc.function_name() << " line " << loc.line() << ": ";
+			std::cout << msg << '\n';
 			((std::cout << std::forward<Ts>(ts) << " "), ...);
 			std::cout << std::endl;
 		}
 	};
 
 	template<typename... Ts>
-	Trace(Ts&&...) -> Trace<Ts...>;
+	Trace(std::string_view msg, Ts&&...) -> Trace<Ts...>;
 };
 
 
